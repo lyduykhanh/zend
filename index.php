f9578fe1ca0a604ffc8808ba3caf73861f622ed8
LY DUY KHANH <lyduykhanh@gmail.com>
2013-06-30 18:16:54 +0700

    Revert "Upload data"
    This reverts commit 496b1788a8379a91a517f23e4b8182352b1fe29e.

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("APPLICATION_PATH",'application');
-set_include_path(APPLICATION_PATH."/../library");
-require_once("Zend/Application.php");
-$app=new Zend_Application("production",APPLICATION_PATH."/configs/application.ini");
-$app->bootstrap()->run();
